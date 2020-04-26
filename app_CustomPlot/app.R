0d0f58b34919eb7c0fe6a1eb7192bc532f143497
Danae Martinez <martinez.danae@rcgt.com>
2020-04-25 21:05:04 -0400

    add of a second panel to explore data

--- app_CustomPlot/app.R
+++ app_CustomPlot/app.R
@@ -3,10 +3,19 @@ library(gapminder)
 library(ggplot2)
 library(colourpicker) 
 library(shinyWidgets)
+library(DT)
+
+my_css <- " 
+#download_data {
+/* Change the background color of the download button to orange. */
+background: orange;
+}
+"
 
 # Define UI for the application
 ui <- fluidPage(
     titlePanel(h2('Make the perfect plot using Shiny')), 
+    tags$style(my_css),
     sidebarLayout(
         sidebarPanel(
             textInput("title", em("Choose a title"), "GDP vs Life Expectancy"),
@@ -18,7 +27,7 @@ ui <- fluidPage(
             colourInput("color", em("Choose the point color"), value ="blue"),
             # Add a continent dropdown selector
             pickerInput("continents", em("Chose the continents"),
-                        choices = levels(gapminder$continent),
+                        choices =  levels(gapminder$continent),
                         multiple = TRUE,
                         selected = "Europe",
                         options = list(style = "btn-danger")), 
@@ -27,22 +36,38 @@ ui <- fluidPage(
                         , max = max(gapminder$year), value = c(1977,2002), sep='', step=1)
         ),
         mainPanel(
-            plotlyOutput("plot", width = 600, height = 600)
+          tabsetPanel(
+            tabPanel("Customize plot",
+             plotlyOutput("plot", width = 600, height = 600)
+            ), 
+            tabPanel("Explore data",
+            sliderInput(inputId = "life", label = "Life expectancy",min = 0, max = 120, value = c(30, 50)),
+            # download button
+            downloadButton(outputId = "download_data", label = "Download"),
+            #tableOutput("table")
+            DT::DTOutput("table")
+            )
+          )
         )
     )
 )
 
 # Define the server logic
-server <- function(input, output) {
-    
-    output$plot <- renderPlotly({
-        
+server <- function(input, output){
+
+data_plot <- reactive({      
+    data <- gapminder %>%
+      filter(continent %in% input$continents & between(year, input$years[1], input$years[2]))  
+  })
+
+data_table <- reactive({
+  data_plot() %>% filter(between(lifeExp, input$life[1], input$life[2]))
+})
+  
+output$plot <- renderPlotly({
         ggplotly({
   
-        data <- gapminder %>% 
-            filter(continent %in% input$continents & between(year, input$years[1], input$years[2]))
-        
-        p <- ggplot(data, aes(gdpPercap, lifeExp)) +
+        p <- ggplot(data_plot(), aes(gdpPercap, lifeExp)) +
              geom_point(size = input$size, col = input$color) +
              scale_x_log10() +
              ggtitle(input$title) +
@@ -56,6 +81,17 @@ server <- function(input, output) {
         p
     })
   })  
+    
+output$table <-  DT::renderDT({
+  data_table()
+})
+
+output$download_data <- downloadHandler(
+  filename <- "gapminder_filtered_data.csv",
+  content <- function(file){
+    data_table()
+  }
+)
     
 }
 
