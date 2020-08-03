89b1deef107024242cc0e278bedbc302a686b9ad
Danae Martínez <martinezvd@dms.umontreal.ca>
2020-08-03 14:26:45 -0400

    organize code

--- app_PopNames/app.R
+++ app_PopNames/app.R
@@ -1,33 +1,46 @@
 
-library(shiny)
-library(ggplot2)
-library(RColorBrewer)
-library(gridExtra)
-library(viridis)
-library(babynames)
-library(dplyr)
-library(DT)
-library(magrittr)
-library(plotly)
-library(shinythemes)
-library(knitr)
-library(kableExtra)
+#----------------------------#
+#---# Popular baby names #---#
+#----------------------------#
 
-babynames <- babynames %>% mutate(year=as.factor(year))
-glimpse(babynames)
+library("shiny")
+library("ggplot2")
+library("RColorBrewer")
+library("gridExtra")
+library("viridis")
+library("babynames")
+library("dplyr")
+library("DT")
+library("magrittr")
+library("plotly")
+library("shinythemes")
+library("knitr")
+library("kableExtra")
+
+babynames <- babynames %>%
+    mutate(year = as.factor(year))
+#glimpse(babynames)
 
 ui <- fluidPage(
-    titlePanel(h1("Most popular names by year")),
-    sidebarLayout(
+    #---# title #---#
+    titlePanel(h1("Most popular baby names by year"))
+    , sidebarLayout(
+        #---# SIDEBARPANEL #---#
         sidebarPanel(
-            selectInput('sex', em('Select Sex'), choices = c("Female" = "F", "Male" = "M")),
-            sliderInput('year', em('Select Year'), min = 1880, max = 2017, value = 1900, sep='')),
-        mainPanel(
+             selectInput(inputId = 'sex'
+                          , label = em('Select Sex')
+                          , choices = c("Female" = "F", "Male" = "M"))
+            , sliderInput(inputId = 'year'
+                          , label = em('Select Year')
+                          , min = 1880
+                          , max = 2017
+                          , value = 1900
+                          , sep=''))
+        #---# MAINPANEL #---#        
+        , mainPanel(
             tabsetPanel(
-                tabPanel('Plot', 
-            plotlyOutput('plot_trendy_names')), 
-                tabPanel('Table', 
-            tableOutput('table_trendy_names')))
+                tabPanel('Plot', plotlyOutput('plot_trendy_names'))
+              , tabPanel('Table', tableOutput('table_trendy_names')))
             ) 
         )
     )
@@ -42,7 +55,7 @@ server <- function(input, output, session) {
             top_n(10, prop) %>% 
             arrange(desc(prop))
         
-        top_10_names$name = factor(top_10_names$name, levels=top_10_names$name)
+        top_10_names$name <- factor(top_10_names$name, levels = top_10_names$name)
         return(top_10_names)
     }
     
@@ -50,9 +63,11 @@ server <- function(input, output, session) {
         
         get_top_names() %>% 
             ggplot(aes(x = name, y = prop, fill= name)) +
-            geom_col(alpha=0.5) +
+            geom_col(alpha = 0.75) +
             scale_fill_viridis(option = "D", discrete = TRUE) +
-            theme(legend.position='none', axis.text.x = element_text(angle=90), axis.title.y = element_text(angle=0)) 
+            theme(legend.position='none'
+                  , axis.text.x = element_text(angle=90)
+                  , axis.title.y = element_text(angle=0)) 
         
     })  
     
@@ -60,17 +75,14 @@ server <- function(input, output, session) {
         
         get_top_names() %>% 
             select(name, n, prop) %>%  
-            #mutate(name = cell_spec(name, color = "white", bold = T
-            #       ,background = spec_color(1:10, begin = 0.25, end = 0.5, option = "A", direction = -1))) %>% 
             set_colnames(c('Name', 'N', 'Prop.')) %>% 
             kable(format = "html"
-                  ,caption = "The most popular names for the selected year and sex"
-                  ,digits = 2) %>%
+                  , caption = "The most popular names for the selected year and sex"
+                  , digits = 2) %>%
             kable_styling(full_width = F
-                          ,bootstrap_options = "striped"
-                          ,position = "left") %>% column_spec(2:3, bold = T)
-        
-        
+                          , bootstrap_options = "striped"
+                          , position = "left") %>% 
+            column_spec(2:3, bold = T)
     })
     
 }
