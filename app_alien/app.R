426672fede45688002a9a66ae95d07f5dcfc8140
Danae Martínez <martinezvd@dms.umontreal.ca>
2020-08-03 13:48:59 -0400

    code organization

--- app_alien/app.R
+++ app_alien/app.R
@@ -1,43 +1,48 @@
 
-library(shiny)
-library(ggplot2)
-library(dplyr)
-library(magrittr)
-library(plotly)
-library(wesanderson)
-library(RColorBrewer)
-library(knitr)
-library(viridis)
-library(kableExtra)
-library(gridExtra)
+#-----------------------------------#
+#---# Alien Sightings Dashboard #---#
+#-----------------------------------#
 
-usa_ufo_sightings <- read.csv('C:/Git/shiny-apps/app_alien/usa_ufo_sightings.csv')
+library("shiny")
+library("ggplot2")
+library("dplyr")
+library("magrittr")
+library("plotly")
+library("wesanderson")
+library("RColorBrewer")
+library("knitr")
+library("viridis")
+library("kableExtra")
+library("gridExtra")
+source('aux_fun.R')
 
-ufo_sightings <- usa_ufo_sightings %>% mutate(date_sighted = as.Date(date_sighted, format = "%Y-%m-%d"))
+usa_ufo_sightings <- read.csv('usa_ufo_sightings.csv')
 
+ufo_sightings <- usa_ufo_sightings %>% 
+    mutate(date_sighted = as.Date(date_sighted, format = "%Y-%m-%d"))
 
 ui <- fluidPage(
-    # Add a title
-    titlePanel('UFO Sightings'), 
-    
-    sidebarLayout(
-        sidebarPanel(
-            p('The National UFO Reporting Center (NUFORC) has collected sightings data throughout the last century.
-            This app allows users to select a U.S. state and a time period in which the sightings occurred.'),
-            
-            # One input to select a U.S. state
-            selectInput('state', 'Choose a U.S. state:', choices = levels(ufo_sightings$state)),
-            # And one input to select a range of dates
-            dateRangeInput("dates",'Choose a date range:'
-                           , start = min(ufo_sightings$date_sighted)
-                           , end = max(ufo_sightings$date_sighted))
-        ),
-        mainPanel(
-          tabsetPanel(
-                tabPanel('Plot',
-                    plotOutput('shapes')),
-                tabPanel('Table',
-                    tableOutput('duration_table'))
+    titlePanel('UFO Sightings')
+    , sidebarLayout(
+        #---# SIDEBAR #---#
+        sidebarPanel(p(intro)
+                     , br()
+                     , p(intro2)
+                     , br()
+                     #---# select a U.S. state #---#
+                     , selectInput(inputId = 'state'
+                                   , label = 'Choose a U.S. state '
+                                   , choices = levels(ufo_sightings$state))
+                    #---# select a range of dates #---#
+                    , dateRangeInput(inputId = "dates"
+                                     , label = 'Choose a date range'
+                                     , start = min(ufo_sightings$date_sighted)
+                                     , end = max(ufo_sightings$date_sighted)))
+        #---# MAIN PANEL #---#
+        , mainPanel(
+            tabsetPanel(
+                tabPanel('Plot', plotOutput('shapes')),
+                tabPanel('Table', tableOutput('duration_table'))
             )
         )
     )
@@ -49,33 +54,32 @@ server <- function(input, output) {
     shapes_summ <- function(){
         
         ufo_sightings_summ <- ufo_sightings %>%
-            filter(state==input$state & between(date_sighted
-                                                , as.Date(input$dates[1], format = "%Y-%m-%d")
-                                                , as.Date(input$dates[2], format = "%Y-%m-%d"))) %>% 
+            filter(state == input$state & between(date_sighted
+                                                  , as.Date(input$dates[1], format = "%Y-%m-%d")
+                                                  , as.Date(input$dates[2], format = "%Y-%m-%d"))) %>% 
             group_by(shape) %>%
             mutate(avg = mean(duration_sec)
-                   ,med=median(duration_sec)
-                   ,min=min(duration_sec)
-                   ,max=max(duration_sec)
-                   ,n =n()) %>%  
-            select(shape, avg,med,min, max,n) %>% 
+                   , med = median(duration_sec)
+                   , min = min(duration_sec)
+                   , max = max(duration_sec)
+                   , n = n()) %>%  
+            select(shape, avg, med, min, max, n) %>% 
             distinct() %>% 
             arrange(desc(n))
         
-        ufo_sightings_summ$shape = factor(ufo_sightings_summ$shape, levels=ufo_sightings_summ$shape)
-        
+        ufo_sightings_summ$shape <- factor(ufo_sightings_summ$shape, levels = ufo_sightings_summ$shape)
         return(ufo_sightings_summ)
+        
     }
-    
+
     output$shapes <- renderPlot({ 
         
         shapes_summ() %>% 
             ggplot(aes(x = shape, y = n, fill = shape)) +
-            geom_col(alpha=0.5) + 
+            geom_col(alpha = 0.75) + 
             guides(fill = FALSE) +
             scale_fill_viridis(option = "C", discrete = TRUE) +
             theme(axis.text.x = element_text(angle=90), axis.title.y = element_text(angle=0)) 
-
         
         })
     
@@ -83,16 +87,13 @@ server <- function(input, output) {
         
         head(shapes_summ()) %>% 
         set_colnames(c("Shape", "Avg.", "Med.", "Min.", "Max.", "N")) %>%  
-        kable(caption = "Average, median, minimum, and maximum duration of the sightings and number of sightings by shape"
-             ,digits = 2
-             ) %>% 
+        kable(caption = table_cap, digits = 2) %>% 
         kable_styling(full_width = F
-                     ,bootstrap_options = "striped"
-                     ,position = "left") 
+                     , bootstrap_options = "striped"
+                     , position = "left") 
         
         }
 
 }
 
 shinyApp(ui, server)
-
---
+++ app_alien/aux_fun.R
@@ -0,0 +1,8 @@
+
+intro <- 'The National UFO Reporting Center (NUFORC) has collected sightings data throughout the last century.
+          This app allows users to select a U.S. state and a time period in which the sightings occurred.'
+
+intro2 <- "The plot corresponds to the number of sights by shape. The table shows descriptive statistics of the 
+           sights's duration by shape for the selected state and range of dates." 
+
+table_cap <- "Average, median, minimum, and maximum duration of the sightings and number of sightings by shape"
